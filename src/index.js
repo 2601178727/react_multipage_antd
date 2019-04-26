0351df00166d12a0d4fae65bacc52a4000f65cf2
YYY <2601178727@qq.com>
2019-04-26 09:23:25 +0800

    save

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ export default class Home extends React.Component
     render()
     {
         return (
-            <Steps direction="vertical" size="small" current={1}>
+            <Steps direction="vertical" size="small" current={3}>
                 <Step title="修改webpack" description="webpack.config.js 增加入口文件" />
                 <Step title="编写代码" description="文件名与入口文件一致" />
                 <Step title="重启服务" description="新文件加入后需要重启服务才能使用热更新" />
                 <Step title="访问地址" description="http://localhost:3000/[pathName]" />
+                <Step title="UI文档" description="https://ant.design/components/steps-cn/" />
+                <Step title="UI设计" description="https://lanhuapp.com/web/#/item/project/board?pid=4be4b486-e73a-4fc8-af34-4d4772bb4be0" />
             </Steps>
         );
     }
