50211b9ade5992d8e9ee1179c45cfe018595691d
YYY <2601178727@qq.com>
2019-04-26 09:20:22 +0800

    save

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ export default class Home extends React.Component
     render()
     {
         return (
-            <Steps direction="vertical" current={1}>
-                <Step title="Finished" description="This is a description." />
-                <Step title="In Progress" description="This is a description." />
-                <Step title="Waiting" description="This is a description." />
+            <Steps direction="vertical" size="small" current={1}>
+                <Step title="修改webpack" description="webpack.config.js 增加入口文件" />
+                <Step title="编写代码" description="文件名与入口文件一致" />
+                <Step title="重启服务" description="新文件加入后需要重启服务才能使用热更新" />
+                <Step title="访问地址" description="http://localhost:3000/[pathName]" />
             </Steps>
         );
     }
